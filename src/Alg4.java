fdafae226b2f7d38809a8d401a0c0b06abec815a
eduardocf2003 <florianoeduardo03@gmail.com>
2022-08-19 17:20:43 -0300

    Alg4 (terminar)

--- src/Alg4.java
+++ src/Alg4.java
@@ -2,6 +2,19 @@ import java.util.Scanner;
 
 public class Alg4 {
     public static void main(String[] args) {
+        float numeroescolhido;
+        int multiplicador = 0;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite o número para o calculo da tabuada: ");
+        numeroescolhido = teclado.nextFloat();
+
+        System.out.println("Tabuada do + e - para o número 5");
         
+        while(multiplicador < 10) {
+        System.out.print(numeroescolhido + " + " + multiplicador + " " + "= " + (numeroescolhido + multiplicador));
+        System.out.println("  " + numeroescolhido + " - " + multiplicador + " " + "= " + (numeroescolhido - multiplicador));
+        multiplicador++;
+        }
     }
 }
